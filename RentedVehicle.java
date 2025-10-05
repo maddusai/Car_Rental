95b0e379ef6f4fe51aa35dd7e1d42d52010f824a
maddusai <saimaddu72@gmail.com>
2025-10-05 17:52:39 +0530

    Update RentedVehicle.java

--- RentedVehicle.java
+++ RentedVehicle.java
@@ -1,44 +1,23 @@
 public class RentedVehicle {
     private Vehicle vehicle;
     private Customer customer;
-    private int duration;
-    private double totalCost;
-    private String rentalType;
-    private boolean isPaid;
-    private PaymentMethod paymentMethod;
+    private int days;
 
-    public enum PaymentMethod {
-        CREDIT_CARD, DEBIT_CARD, CASH, DIGITAL_WALLET
-    }
-
-    public RentedVehicle(Vehicle vehicle, Customer customer, int duration, 
-                         double totalCost, String rentalType, PaymentMethod paymentMethod) {
+    public RentedVehicle(Vehicle vehicle, Customer customer, int days) {
         this.vehicle = vehicle;
         this.customer = customer;
-        this.duration = duration;
-        this.totalCost = totalCost;
-        this.rentalType = rentalType;
-        this.paymentMethod = paymentMethod;
-        this.isPaid = false;
+        this.days = days;
+        this.vehicle.setAvailable(false);
     }
 
-    public boolean processPayment() {
-        System.out.println("Processing Payment...");
-        System.out.println("Payment Method: " + paymentMethod);
-        System.out.println("Total Amount: $" + totalCost);
-        
-        isPaid = true;
-        System.out.println("Payment Successful!");
-        return true;
+    public double getTotalPrice() {
+        return vehicle.getPricePerDay() * days;
+    }
+
+    public String getReceipt() {
+        return customer.getName() + " rented " + vehicle.getBrand() + " " + vehicle.getModel() +
+               " for " + days + " days. Total: $" + getTotalPrice();
     }
 
     public Vehicle getVehicle() { return vehicle; }
-    public Customer getCustomer() { return customer; }
-    public int getDuration() { return duration; }
-    public double getTotalCost() { return totalCost; }
-    public String getRentalType() { return rentalType; }
-    public boolean isPaid() { return isPaid; }
-    public String getFormattedDuration() {
-        return duration + " " + (rentalType.equals("daily") ? "day(s)" : "hour(s)");
-    }
 }
