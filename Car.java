82eb2b2ac233e757e6c6cef6f15e68771149e2c8
maddusai <saimaddu72@gmail.com>
2025-10-05 17:49:24 +0530

    Update Car.java

--- Car.java
+++ Car.java
@@ -1,23 +1,13 @@
 public class Car extends Vehicle {
-    private int doors;
     private int seats;
-    private boolean isLuxury;
-    private String fuelType;
 
-    public Car(String brand, String model, int year, double pricePerDay,
-               int doors, int seats, boolean isLuxury, String fuelType) {
+    public Car(String brand, String model, int year, double pricePerDay, int seats) {
         super(brand, model, year, pricePerDay);
-        this.doors = doors;
         this.seats = seats;
-        this.isLuxury = isLuxury;
-        this.fuelType = fuelType;
     }
 
     @Override
-    public String toString() {
-        return getBrand() + " " + getModel() + " (" + getYear() + ") - " +
-               "$" + getPricePerDay() + "/day, " +
-               doors + " doors, " + seats + " seats, " +
-               (isLuxury ? "Luxury" : "Standard") + ", Fuel: " + fuelType;
+    public String getDetails() {
+        return super.getDetails() + ", Seats: " + seats;
     }
 }
